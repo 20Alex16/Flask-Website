0e80d0e6f5e9b41daa96639ed53b842767a8cad5
20Alex16 <tripaa.alex@gmail.com>
2022-08-26 12:48:17 +0300

    varianta noua

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-print('hello')
-<<<<<<< HEAD
+from flask import Flask, render_template, send_file
 
-=======
-a = 100
->>>>>>> 2e5cca1a06d3e6e6df7729883958cb1946756fa6
+app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return render_template('index.html') # vor fi luate din folderul 'static'
 
-uhufyiouhho
+@app.route('/style.css')
+def style():
+    return send_file('static/style.css')
+
+@app.route('/colorChangingBkgr.js')
+def colorChangingBkgr():
+    return send_file('static/colorChangingBkgr.js')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
