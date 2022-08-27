efed218d4e206372c25d3c0dd63db6e535169253
20Alex16 <tripaa.alex@gmail.com>
2022-08-27 08:47:18 +0300

    organizare

--- main.py
+++ main.py
@@ -1,14 +1,36 @@
-from flask import Flask, render_template, send_file
+from flask import Flask, render_template, send_file, redirect, url_for, request
+import json
 
 app = Flask(__name__, static_folder='scripts')
 
 @app.route('/')
 def index():
-    return render_template('index.html') # vor fi luate din folderul 'static'
+    return redirect('/welcome')
 
-@app.route('/style.css')
-def style():
-    return send_file('scripts/style.css')
+@app.route('/welcome')
+def welcome():
+    return render_template('login.html') # vor fi luate din folderul 'static'
+
+@app.route('/login', methods=['POST'])
+def login():
+    error = None
+    print(request.form)
+    # if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+    #     error = 'Invalid Credentials. Please try again.'
+    # else:
+    #     return redirect(url_for('profile'))
+    return render_template('login.html', error=error)
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+
+
+
+@app.route('/style_login.css')
+def style_login():
+    return send_file('scripts/style_login.css')
 
 @app.route('/colorChangingBkgr.js')
 def colorChangingBkgr():
